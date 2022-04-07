8f4fd8d7e58923fb39b97cce2ccec00657a77156
estarmalkia <estarmalkia@gmail.com>
2022-04-07 13:10:46 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
 /**
 * main - Entry point
-* Return:1
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
