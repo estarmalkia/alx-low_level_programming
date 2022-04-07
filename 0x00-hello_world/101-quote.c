b74a2ddd4c50cb25b8252f754f3dc226aa988363
estarmalkia <estarmalkia@gmail.com>
2022-04-07 12:56:05 -0700

    eightthree

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,24 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
-
 #include <unistd.h>
-
 /**
-
- * main - Entry point
-
- * Return:1
-
- */
-
+* main - Entry point
+* Return:1
+*/
 int main(void)
-
 {
-
 char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
 write(2, ch, sizeof(ch));
-
 return (1);
-
 }
