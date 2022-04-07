af3e419a83c3ce55749a809c64b157629a827659
estarmalkia <estarmalkia@gmail.com>
2022-04-07 12:48:24 -0700

    eightone

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,2 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
-* main - Entry point
-* Return:1
-*/
-int main(void)
-{
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
-return (1);
-}
+#!/bin/bash
+gcc -S -masm=intel $CFILE
