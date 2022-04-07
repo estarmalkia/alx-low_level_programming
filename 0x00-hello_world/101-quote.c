456c335cbb186294dbc1e41e089be730f739a35f
estarmalkia <estarmalkia@gmail.com>
2022-04-07 12:51:48 -0700

    eighttwo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,24 @@
 #!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+
+#include <unistd.h>
+
+/**
+
+ * main - Entry point
+
+ * Return:1
+
+ */
+
+int main(void)
+
+{
+
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, ch, sizeof(ch));
+
+return (1);
+
+}
