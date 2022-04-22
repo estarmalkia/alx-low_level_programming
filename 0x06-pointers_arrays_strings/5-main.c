a8b04dd2c7cbdd436e02ceb51381236a41c19490
estarmalkia <estarmalkia@gmail.com>
2022-04-22 12:50:29 +0300

    more pointers arrays string

---
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  char s[] = "Holberton School!\n";
+  char *ptr;
+
+  p = string_toupper(s);
+  printf("%s", ptr);
+  printf("%s", s);
+  return (0);
+}
---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+  char *ptr;
+
+  p = cap_string(s);
+  printf("%s", ptr);
+  printf("%s", s);
+  return (0);
+}
