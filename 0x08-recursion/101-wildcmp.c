8073112f58cce52632e48594ab3b4638c629ccc4
estarmalkia <estarmalkia@gmail.com>
2022-04-28 23:02:40 +0300

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,13 +9,13 @@ int wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0')
 {
-if (s2 != '\0' && *s2 == '')
+if (*s2 != '\0' && *s2 == '*')
 {
 return (wildcmp(s1, s2 + 1));
 }
 return (*s2 == '\0');
 }
-if (s2 == '')
+if (*s2 == '*')
 {
 return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
